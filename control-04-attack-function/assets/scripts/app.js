2d1d58ede5aa20241459fa41fb6d25f736024fc5
Mohit-kavad <mohitkavad@gmail.com>
2022-08-10 12:52:19 +0530

    strong attack function

--- control-04-attack-function/assets/scripts/app.js
+++ control-04-attack-function/assets/scripts/app.js
@@ -1,4 +1,6 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -6,9 +8,33 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler(){
-  const damage =  dealMonsterDamage(ATTACK_VALUE);
+function attckMonster(mode) {
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(maxDamage);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You Win!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You loss!");
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("You have a draw!");
+  }
 }
 
-attackBtn.addEventListener('click',attackHandler);
+function attackHandler() {
+  attckMonster("ATTACK")
+}
+
+function strongAttackHandler() {
+  attckMonster("STRONG_ATTACK")
+}
+
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
