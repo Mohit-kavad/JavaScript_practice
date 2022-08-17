65b7332c98ad7da444cc37de57475aabb2ab53c7
Mohit-kavad <mohitkavad@gmail.com>
2022-08-17 12:56:42 +0530

    completed functions

--- control-04-attack-function/assets/scripts/app.js
+++ control-04-attack-function/assets/scripts/app.js
@@ -3,80 +3,78 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-const MODE_ATTACK = 'ATTACK';
-const MODE_STRONG_ATTACK ="STRONG_ATTACK"
+const MODE_ATTACK = "ATTACK";
+const MODE_STRONG_ATTACK = "STRONG_ATTACK";
 const LOG_EVENT_PLAYER_ATTACK = "PALYER_ATTACK";
 const LOG_EVENT_PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
 const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
 const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
 const LOG_EVENT_GAME_OVER = "GAME_OVER";
 
-
-const enterdValue = prompt('Maximum life for you an Monster','100');
+const enterdValue = prompt("Maximum life for you an Monster", "100");
 
 let chosenMaxLife = parseInt(enterdValue);
 // let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
-let battleLog =[];
+let battleLog = [];
 
-
-if(isNaN(chosenMaxLife) || chosenMaxLife <= 0 ){
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
   chosenMaxLife = 100;
 }
 
 adjustHealthBars(chosenMaxLife);
 
-function writeToLog(event,val,monsterHealth,playerHealth){
+function writeToLog(event, val, monsterHealth, playerHealth) {
   let logEntry;
-  if(event === LOG_EVENT_PLAYER_ATTACK){
-    logEntry={
-      event : event,
-      value : val,
-      target : 'MONSTER',
-      finalMonsterHealth : monsterHealth,
-      finalPlayerHealth : playerHealth
-    }
-  }else if(event === LOG_EVENT_PLAYER_STRONG_ATTACK){
-    logEntry={
-      event : event,
-      value : val,
-      target : 'MONSTER',
-      finalMonsterHealth : monsterHealth,
-      finalPlayerHealth : playerHealth
-    }
-  }else if(event === LOG_EVENT_MONSTER_ATTACK){
-    logEntry={
-      event : event,
-      value : val,
-      target : 'PLAYER',
-      finalMonsterHealth : monsterHealth,
-      finalPlayerHealth : playerHealth
-    }
-  }else if(event === LOG_EVENT_PLAYER_HEAL){
-    logEntry={
-      event : event,
-      value : val,
-      target : 'PLAYER',
-      finalMonsterHealth : monsterHealth,
-      finalPlayerHealth : playerHealth
-    }
-  }else if(event === LOG_EVENT_GAME_OVER){
-    logEntry={
-      event : event,
-      value : val,
-      finalMonsterHealth : monsterHealth,
-      finalPlayerHealth : playerHealth
-    }
+  if (event === LOG_EVENT_PLAYER_ATTACK) {
+    logEntry = {
+      event: event,
+      value: val,
+      target: "MONSTER",
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
+  } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+    logEntry = {
+      event: event,
+      value: val,
+      target: "MONSTER",
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
+  } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+    logEntry = {
+      event: event,
+      value: val,
+      target: "PLAYER",
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
+  } else if (event === LOG_EVENT_PLAYER_HEAL) {
+    logEntry = {
+      event: event,
+      value: val,
+      target: "PLAYER",
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
+  } else if (event === LOG_EVENT_GAME_OVER) {
+    logEntry = {
+      event: event,
+      value: val,
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
   }
   battleLog.push(logEntry);
 }
 
-function reset(){
+function reset() {
   currentMonsterHealth = chosenMaxLife;
   currentPlayerHealth = chosenMaxLife;
-  resetGame(chosenMaxLife)
+  resetGame(chosenMaxLife);
 }
 
 function endRound() {
@@ -89,50 +87,52 @@ function endRound() {
     playerDamage,
     currentMonsterHealth,
     currentPlayerHealth
-  )
+  );
 
   if (currentPlayerHealth <= 0 && hasBonusLife) {
-    hasBonusLife = false ;
+    hasBonusLife = false;
     removeBonusLife();
     currentPlayerHealth = initialPlayerHealth;
     setPlayerHealth(initialPlayerHealth);
-    alert('you would be a dead but bonus life saved you!')
+    alert("you would be a dead but bonus life saved you!");
   }
 
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("You Win!");
     writeToLog(
       LOG_EVENT_GAME_OVER,
-      'PALAYER_WON',
+      "PALAYER_WON",
       currentMonsterHealth,
       currentPlayerHealth
-    )
+    );
     reset();
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert("You loss!");
     writeToLog(
       LOG_EVENT_GAME_OVER,
-      'MONSTER_WON!',
+      "MONSTER_WON!",
       currentMonsterHealth,
       currentPlayerHealth
-    )
+    );
     reset();
   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert("You have a draw!");
     writeToLog(
       LOG_EVENT_GAME_OVER,
-      'A DRAW!',
+      "A DRAW!",
       currentMonsterHealth,
       currentPlayerHealth
-    )
+    );
     reset();
   }
-  
 }
 
 function attckMonster(mode) {
   const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
-  let logEvent = mode === MODE_ATTACK ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK;
+  let logEvent =
+    mode === MODE_ATTACK
+      ? LOG_EVENT_PLAYER_ATTACK
+      : LOG_EVENT_PLAYER_STRONG_ATTACK;
   // if (mode === MODE_ATTACK) {
   //   maxDamage = ATTACK_VALUE;
   //   logEvent = LOG_EVENT_PLAYER_ATTACK
@@ -142,15 +142,10 @@ function attckMonster(mode) {
   // }
   const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
-  writeToLog(
-    logEvent,
-    damage,
-    currentMonsterHealth,
-    currentPlayerHealth
-  )
+  writeToLog(logEvent, damage, currentMonsterHealth, currentPlayerHealth);
   endRound();
 }
-  
+
 function attackHandler() {
   attckMonster(MODE_ATTACK);
 }
@@ -174,15 +169,15 @@ function healPlayerHendler() {
     healvalue,
     currentMonsterHealth,
     currentPlayerHealth
-  )
+  );
   endRound();
 }
 
-function printLogHendler(){
+function printLogHendler() {
   console.log(battleLog);
 }
 
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 healBtn.addEventListener("click", healPlayerHendler);
-logBtn.addEventListener('click',printLogHendler)
+logBtn.addEventListener("click", printLogHendler);
--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,1 +1,73 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_PLAYER_WIN = "PLAYER_WIN";
+const RESULT_COMPUTER_WIN = "COMPUTER_WIN";
+const RESULT_DRAW = "DRAW";
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK},${PAPER} OR ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice!we chose ${DEFAULT_USER_CHOICE} for you!`);
+    return ;
+  }
+  return selection;
+};
+
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = (cChoice, pChoice) => {
+  // condition for player win
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WIN;
+  } else {
+    return RESULT_COMPUTER_WIN;
+  }
+};
+
+startGameBtn.addEventListener("click", function () {
+//   if (gameIsRunning) {
+//     // gameIsRunning === true
+//     return;
+//   }
+//   gameIsRunning = true;
+  console.log("game is starting....");
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice();
+  // console.log(computerSelection);
+  const winner = getWinner(computerSelection, playerSelection);
+  // console.log(winner);
+  let message = `you picked ${playerSelection} ,computer picked ${computerSelection} therefore you`;
+  if(winner === RESULT_DRAW){
+    message = message + ' had a draw';
+  }else if(winner === RESULT_PLAYER_WIN){
+    message = message + ' won';
+  }else{
+    message = message + ' loss'
+  }
+  alert(message);
+
+});
